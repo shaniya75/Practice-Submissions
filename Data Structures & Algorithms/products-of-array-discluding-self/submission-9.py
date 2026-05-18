bdebd9d4d6a777d6ac37f0fc0b69f765863a960b
shaniya75 <shaniyavarghese75@gmail.com>
2026-05-18 17:51:29 +0530

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.py
@@ -0,0 +1,26 @@
+import math
+from collections import defaultdict
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        D=defaultdict(list)
+        dup=[]
+        res=[]
+        for i in range(len(nums)):
+            #a=nums.index(i)
+            for j in range(i+1, len(nums)):
+                dup.append(nums[j])
+            for k in range(i-1,-1,-1):
+                dup.append(nums[k])
+            D[i]=math.prod(dup)
+            dup=[]
+
+        for i in D:
+            res.append(D[i])
+        return res
+        
+            
+     
+
+        
+
+        
